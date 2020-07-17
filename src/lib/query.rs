ec887d028105a2006a505d76f9adc8884efbd641
Rheydskey <matgag02@gmail.com>
2020-07-17 14:03:42 +0200

    Add -R to seach in repository

--- src/lib/query.rs
+++ src/lib/query.rs
@@ -4,7 +4,7 @@ pub fn query(packages: Vec<String>) {
     if packages.len() != 0 {
         Command::new("sudo")
             .arg("xbps-query")
-            .arg("-s")
+            .arg("-R -s")
             .arg(&packages[0])
             .status()
             .expect("failed to execute process");
