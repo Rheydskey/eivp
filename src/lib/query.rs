37b20ac45cbe3de54408108b0f16357de9e07de3
Rheydskey <matgag02@gmail.com>
2020-07-17 14:04:47 +0200

    fix a bug

--- src/lib/query.rs
+++ src/lib/query.rs
@@ -4,7 +4,8 @@ pub fn query(packages: Vec<String>) {
     if packages.len() != 0 {
         Command::new("sudo")
             .arg("xbps-query")
-            .arg("-R -s")
+            .arg("-R")
+            .arg("-s")
             .arg(&packages[0])
             .status()
             .expect("failed to execute process");
