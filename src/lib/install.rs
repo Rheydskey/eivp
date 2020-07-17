ad7bf53e655eafebf341fbb8305d817b6f86e19d
Rheydskey <matgag02@gmail.com>
2020-07-17 11:19:02 +0200

    Install working for repo packages

--- src/lib/install.rs
+++ src/lib/install.rs
@@ -1,8 +1,13 @@
-use std::process::Command;
+use runas::Command;
 
 pub fn install(packages: Vec<String>) {
-            Command::new("xbps-install")
-                .arg(&packages[0])
-                .output()
-                .expect("failed to execute process");
+    if packages.len() != 0 {
+        Command::new("sudo")
+            .arg("xbps-install")
+            .arg(&packages[0])
+            .status()
+            .expect("failed to execute process");
+    } else {
+        println!("Package needed")
+    }
 }
