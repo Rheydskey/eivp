995b8babd976cf12e163f59b71f8de5bc4805c74
Rheydskey <matgag02@gmail.com>
2020-07-17 13:59:14 +0200

    And adds packet search

---
+++ src/lib/query.rs
@@ -0,0 +1,14 @@
+use runas::Command;
+
+pub fn query(packages: Vec<String>) {
+    if packages.len() != 0 {
+        Command::new("sudo")
+            .arg("xbps-query")
+            .arg("-s")
+            .arg(&packages[0])
+            .status()
+            .expect("failed to execute process");
+    } else {
+        println!("Package needed")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod lib;
 use structopt::StructOpt;
 use crate::lib::install::install;
 use crate::lib::update::update;
+use crate::lib::query::query;
+use crate::lib::remove::remove;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "A easily installer for void packages", version="0.0.1")]
 enum Opt {
@@ -29,8 +31,8 @@ fn main() {
     let opt = Opt::from_args();
     match opt {
         Opt::Install { package_name } => { install(package_name); },
-        Opt::Query { package_name } => {print!("{:?}", package_name[0])},
-        Opt::Remove { package_name } => {print!("{:?}", package_name[0])},
+        Opt::Query { package_name } => {query(package_name)},
+        Opt::Remove { package_name } => {remove(package_name)},
         Opt::Update {} => {update()}
     }
 }
