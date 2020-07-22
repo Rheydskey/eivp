2c021a0fe15e0874f239b15fb0479326a954a2f9
mateo gagnon <mateo@mateo-pc>
2020-07-22 09:56:16 +0200

    Removing Some Files Not Removed by git

--- src/lib/update.rs
+++
@@ -1,9 +0,0 @@
-use runas::Command;
-
-pub fn update() {
-    Command::new("sudo")
-            .arg("xbps-install")
-            .arg("-Syu")
-            .status()
-            .expect("failed to execute process");
-}
